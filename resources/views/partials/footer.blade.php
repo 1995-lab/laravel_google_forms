e7bd906ffb87e81cab71eb846eac35bc2bd1a9fe
Ousmane Traoré <ousmanetra1995@gmail.com>
2020-12-23 13:28:28 +0100

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer text-muted text-center{{ !empty($class) ? " {$class}" : ''  }}">
+    &copy; {{ date('Y') }} <a href="{{ route('forms.index') }}">{{ config('app.name') }}</a> by
+     <a href="{{ config('custom.app.owner.url') }}" target="_blank">
+     ouz </a>
+</div>
